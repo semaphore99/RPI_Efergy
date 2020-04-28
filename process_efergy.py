24bc3975a76dd3c44fcd372758f11525cf9dca58
dixon <dixond@internode.on.net>
2020-04-28 21:10:49 +1000

    upload to mongoatlas. daily data is kept in an array where each reading is an array entry with a timestamp. also put mongo credentials in separate secret file

--- process_efergy.py
+++ process_efergy.py
@@ -1,12 +1,32 @@
 import sys
 import pymongo
+from datetime import datetime
 
+secret = open("secret", "r")
+credentials = secret.read()
 
-# connect to Mongo
-#client = pymongo.MongoClient("mongodb+srv://dbWriter:<password>@cluster0-scaef.mongodb.net/test?retryWrites=true&w=majority")
-#db = client.test
+client = pymongo.MongoClient("mongodb+srv://" + credentials + "@cluster0-i2cea.mongodb.net/test?retryWrites=true&w=majority")
+efergyDB = client.Efergy
+testCollection = efergyDB.test
 
-
+thisHour = 0
+readings = []
 for line in sys.stdin:
-    sys.stdout.write("process_efergy...hello!")
-    sys.stdout.write(line)
+    # example line: 04/26/20,21:57:48,2603.437500
+    now = datetime.now()
+    print("Processing: " + line)
+    tokens = line.split(",")
+    docId = str(now.year) + str(now.month) + str(now.day)
+    
+    timestamp = now.strftime("%Y%m%d-%H%M%S")
+    #really only interested in the last number
+    reading = {'timestamp':timestamp, 'reading':tokens[-1]}
+    readings.append(reading)
+    document = {
+        '_id': docId,
+        'readings': readings
+    }
+    key = {'_id': docId}
+    testCollection.replace_one(key, document, upsert=True)
+
+
